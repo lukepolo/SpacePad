17ca67f9b01a50e475a6f47f79212b3c43755782
Luke Policinski <luke.policinski@egov.com>
2017-10-17 16:34:38 -0400

    fixed nav

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -5,12 +5,22 @@
                 <router-link to="/" class="navbar-item">
                     Space Pad
                 </router-link>
-                <router-link :to="{ name : 'my-profile' }" class="navbar-item" style="margin-left: auto;">
+                <router-link :to="{ name : 'my-profile' }" class="navbar-item is-hidden-desktop" style="margin-left: auto;">
                     My Profile
                 </router-link>
-                <a class="navbar-item" @click="logout">
+                <a class="navbar-item is-hidden-desktop" @click="logout">
                     Logout
                 </a>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-end">
+                    <router-link :to="{ name : 'my-profile' }" class="navbar-item" style="margin-left: auto;">
+                        My Profile
+                    </router-link>
+                    <a class="navbar-item" @click="logout">
+                        Logout
+                    </a>
+                </div>
             </div>
         </nav>
         <section v-if="current_version !==  version">
